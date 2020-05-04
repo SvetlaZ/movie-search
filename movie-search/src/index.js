ffe043f1877dc6cce884ed54256a4e3b5356fe96
Svetlana <svezan@mail.ru>
2020-05-04 22:10:08 +0300

    feat: add slider

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,4 +1,27 @@
 import './style.scss';
+import Swiper from 'swiper';
+
+const mySwiper = new Swiper('.swiper-container', {
+
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+mySwiper.appendSlide([
+  '<div class="swiper-slide">Slide 1</div>',
+  '<div class="swiper-slide">Slide 2</div>',
+]);
 
 async function getMovieTitle(nameMovie) {
   const url = `https://www.omdbapi.com/?s=${nameMovie}&apikey=264bae6c`;
@@ -7,23 +30,11 @@ async function getMovieTitle(nameMovie) {
   const data = await res.json();
 
   // console.log(data.Search[0].Title);
+  console.log(data.Search[0].Poster);
   console.log(data.Search);
 
   return data;
 }
 
-async function getPoster(nameMovie) {
-  const url = `https://img.omdbapi.com/?s=${nameMovie}&apikey=264bae6c`;
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-  // console.log(data.Search[0].Title);
-
-  
-  console.log(data.Search);
-
-  return data;
-}
 
 getMovieTitle('cat');
