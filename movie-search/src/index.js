f6f3ee1667910f20a51cac5072afdc01c87f5306
Svetlana <svezan@mail.ru>
2020-05-04 16:12:22 +0300

    feat: add requests for posters and movie titles

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,1 +1,29 @@
 import './style.scss';
+
+async function getMovieTitle(nameMovie) {
+  const url = `https://www.omdbapi.com/?s=${nameMovie}&apikey=264bae6c`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  // console.log(data.Search[0].Title);
+  console.log(data.Search);
+
+  return data;
+}
+
+async function getPoster(nameMovie) {
+  const url = `https://img.omdbapi.com/?s=${nameMovie}&apikey=264bae6c`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  // console.log(data.Search[0].Title);
+
+  
+  console.log(data.Search);
+
+  return data;
+}
+
+getMovieTitle('cat');
