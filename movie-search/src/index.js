3c671b578a47d03f762e8ee9f83453393359a2f5
Svetlana <svezan@mail.ru>
2020-05-05 21:01:16 +0300

    feat: add create a movie-card

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -4,7 +4,8 @@ import Swiper from 'swiper';
 const mySwiper = new Swiper('.swiper-container', {
 
   direction: 'horizontal',
-  loop: true,
+  // loop: true,
+  initialSlide: 1,
 
   pagination: {
     el: '.swiper-pagination',
@@ -18,23 +19,73 @@ const mySwiper = new Swiper('.swiper-container', {
   },
 });
 
+mySwiper.slideTo(-1);
+
 mySwiper.appendSlide([
-  '<div class="swiper-slide">Slide 1</div>',
-  '<div class="swiper-slide">Slide 2</div>',
+  '<div class="swiper-slide slide-1"></div>',
+  '<div class="swiper-slide slide-2"></div>',
+  '<div class="swiper-slide slide-3"></div>',
+  '<div class="swiper-slide slide-4"></div>',
+  '<div class="swiper-slide slide-5"></div>',
 ]);
+
+// в отдельный модуль
+function createBlock(type, classname, innerText) {
+  const newBlock = document.createElement(type);
+  newBlock.classList.add(classname);
+  newBlock.innerText = innerText;
+
+  return newBlock;
+}
+
+const swipperWrapper = document.querySelector('.slide-1');
+// swipperWrapper = document.querySelector('.slide-2');
+
+
+// в отдельный модуль
+function createCard(title, poster, year, rating) {
+  const card = createBlock('div', 'card', '');
+  swipperWrapper.append(card);
+
+  const cardName = createBlock('p', 'card-name', title);
+  card.append(cardName);
+
+  const cardPoster = createBlock('div', 'card-poster', '');
+  cardPoster.style = `background-image: url(${poster})`;
+  card.append(cardPoster);
+
+  const cardYear = createBlock('p', 'card-year', year);
+  card.append(cardYear);
+
+  const cardRaiting = createBlock('p', 'card-raiting', rating);
+  card.append(cardRaiting);
+
+  mySwiper.update();
+  mySwiper.navigation.update();
+}
 
 async function getMovieTitle(nameMovie) {
   const url = `https://www.omdbapi.com/?s=${nameMovie}&apikey=264bae6c`;
 
-  const res = await fetch(url);
-  const data = await res.json();
+  fetch(url)
+    .then((response) => response.json())
+    .then(({ Search }) => {
+      console.log(Search);
+      fetch(`https://www.omdbapi.com/?i=${Search[0].imdbID}&apikey=264bae6c`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.Title);
+          console.log(data.Year);
+          console.log(data.Poster);
+          console.log(data.imdbRating);
+          createCard(data.Title, data.Poster, data.Year, data.imdbRating);
+        });
+    });
 
-  // console.log(data.Search[0].Title);
-  console.log(data.Search[0].Poster);
-  console.log(data.Search);
-
-  return data;
+// mySwiper.appendSlide([
+//   `<div class="swiper-slide">${data.Search[0].Title}</div>`,
+// ]);
 }
 
-
-getMovieTitle('cat');
+// const request = document.querySelector('#search').value;
+getMovieTitle('love');
